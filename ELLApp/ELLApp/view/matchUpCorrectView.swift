9c5af48480b9b288201b45abfe29417426732d2d
Joseph <mcgeevej@uni.coventry.ac.uk>
2020-02-21 13:33:00 +0000

    audio succèss and segue is correct to get back from match up

--- ELLApp/ELLApp/view/matchUpCorrectView.swift
+++ ELLApp/ELLApp/view/matchUpCorrectView.swift
@@ -13,9 +13,12 @@ import AVFoundation
 class correctMatch : UIViewController {
     
     var colour : String = ""
+    var completed : Bool = false
+    var player: AVAudioPlayer?
     
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var lastItemLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -29,7 +32,30 @@ class correctMatch : UIViewController {
         
             let synthesizer = AVSpeechSynthesizer()
             synthesizer.speak(utterance)
+        
+        if(completed){
+            lastItemLabel.text = "You have completed the game!"
+            playSound()
+        }
     }
     
+
+    func playSound() { //plays a success sound
+        guard let url = Bundle.main.url(forResource: "levelComplete", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
 }
