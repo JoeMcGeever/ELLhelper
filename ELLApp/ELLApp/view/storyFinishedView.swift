96b961c36a4c0481d8153141ce3f65e406ee08c1
Joseph <mcgeevej@uni.coventry.ac.uk>
2020-02-24 14:49:31 +0000

    added success sound to finished articles

--- ELLApp/ELLApp/view/storyFinishedView.swift
+++ ELLApp/ELLApp/view/storyFinishedView.swift
@@ -8,15 +8,17 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class storyFinished: UIViewController {
     
     let storyData = story()
     //ask a few questions here about the story perhaps
     //load up from coreData - a seperate store should be for the questions
-    
+    var player: AVAudioPlayer?
     var text : String = ""
     @IBOutlet weak var labelForText: UILabel!
+    
     
     
     override func viewDidLoad() {
@@ -24,6 +26,25 @@ class storyFinished: UIViewController {
         // Do any additional setup after loading the view.
         labelForText.text = "Well done on finishing: " + text + "!"
         storyData.updateCompletionOfStory(title: text)
+        playSound()
     }
+    
+     func playSound() { //plays a success sound
+           guard let url = Bundle.main.url(forResource: "levelComplete", withExtension: "mp3") else { return }
+
+           do {
+               try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+               try AVAudioSession.sharedInstance().setActive(true)
+
+               player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+               guard let player = player else { return }
+
+               player.play()
+
+           } catch let error {
+               print(error.localizedDescription)
+           }
+       }
     
 }
