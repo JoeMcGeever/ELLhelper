7efb44abb70e27de0f3239b760c1026fe71670c7
Joseph <mcgeevej@uni.coventry.ac.uk>
2020-02-28 21:04:59 +0000

    began working on the translation API part

--- ELLApp/ELLApp/model/wordBank.swift
+++ ELLApp/ELLApp/model/wordBank.swift
@@ -32,7 +32,7 @@ class WordBank  {
     }
     
     func saveNewWordToCoreData(word : String) {
-        
+
     }
     func saveImageToWord(word : String, image : UIImage){
         //recieves the updated
@@ -43,6 +43,65 @@ class WordBank  {
         return[Word(EnglishWord: "test"), Word(EnglishWord: "test2"), Word(EnglishWord: "test3")]
     }
     
+    
+    
+    
+    
+    
+    //below is teh strutcure for an API async -- might not be the same for google API but it is a start
+    
+    
+    
+    
+    //   https://www.appcoda.com/google-translation-api/
+    
+    
+    
+    struct Response : Codable{ //this is probably the structure of the response sent by the API
+        let word : String
+        let translation : String
+    }
+    
+    
+    func getTranslation(word:String, completionHandler: @escaping (Array<String>) -> ()) {
+        //an API request to return a tran of a selection of questions
+        let headers = [
+            "":
+            ""
+        ]
+        let urlString = "https://something\(word)" //inject into the url
+        let request = NSMutableURLRequest(url: NSURL(string: urlString)! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET" //GET request
+        request.allHTTPHeaderFields = headers //include the headers (including my API key)
+
+        let session = URLSession.shared
+        session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) in //async func here URLSession  { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error!)
+
+                do{
+                    let jsonDecoder = JSONDecoder()
+                    let dataObject = try jsonDecoder.decode(Response.self, from: data!)
+                    //print("JSON decoded here:")
+                    //print(dataObject)
+                    
+                    
+      //              let translation = dataObject.typeOf
+      //              completionHandler(translation) //return in the completion handler --> callback
+                    
+                    
+                }
+                catch {
+                    print("Failed decoding")
+                    completionHandler([])
+                    
+                }
+            }
+            }).resume()
+    
+    }
     
 }
 
